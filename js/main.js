2f4d2dc36d5f5c103233397c0c01b6c19aa5463b
Pilow <pierre.laupretre@gmail.com>
2016-09-20 00:02:34 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+ console.log('Service Worker is supported');
+ navigator.serviceWorker.register('sw.js').then(function(reg) {
+   console.log(':^)', reg);
+   reg.pushManager.subscribe({
+        userVisibleOnly: true
+    }).then(function(sub) {
+        console.log('endpoint:', sub.endpoint);
+		document.getElementById('code').innerHTML = sub.endpoint
+    });
+ }).catch(function(err) {
+   console.log(':^(', err);
+ });
+}
